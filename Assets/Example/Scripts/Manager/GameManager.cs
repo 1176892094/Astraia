6ec4c58da6d3f791c76447c975a04b78bd58065a
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-04-21 16:45:27 +0800

    案例更新8

--- Assets/Example/Scripts/Manager/GameManager.cs
+++ Assets/Example/Scripts/Manager/GameManager.cs
@@ -96,13 +96,19 @@ namespace Runtime
         {
             if (NetworkManager.Server.connections == 1)
             {
-                AssetManager.Load<GameObject>("Prefabs/SpawnManager", obj => { NetworkManager.Server.Spawn(obj); });
+                AssetManager.Load<GameObject>("Prefabs/SpawnManager", obj =>
+                {
+                    NetworkManager.Server.Spawn(obj);
+                });
             }
         }
 
         public void Execute(ServerReady message)
         {
-            AssetManager.Load<GameObject>("Prefabs/Player", obj => { NetworkManager.Server.Spawn(obj, message.client); });
+            AssetManager.Load<GameObject>("Prefabs/Player", obj =>
+            {
+                NetworkManager.Server.Spawn(obj, message.client);
+            });
         }
     }
 }
