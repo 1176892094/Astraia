c47f5dba53eb80758ea414a6b85f425cd870afd6
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-04-30 23:53:10 +0800

    敏感词过滤器

---
+++ Assets/Astraia/Runtime/01.基础库/16.过滤器/Filter.Node.cs
@@ -0,0 +1,27 @@
+// // *********************************************************************************
+// // # Project: JFramework
+// // # Unity: 6000.3.5f1
+// // # Author: 云谷千羽
+// // # Version: 1.0.0
+// // # History: 2025-04-30 22:04:55
+// // # Recently: 2025-04-30 22:04:55
+// // # Copyright: 2024, 云谷千羽
+// // # Description: This is an automatically generated comment.
+// // *********************************************************************************
+
+using System.Collections.Generic;
+
+namespace Astraia
+{
+    public static partial class Service
+    {
+        public static partial class Filter
+        {
+            private class Node
+            {
+                public readonly Dictionary<char, Node> nodes = new Dictionary<char, Node>();
+                public bool finish;
+            }
+        }
+    }
+}
---
+++ Assets/Astraia/Runtime/01.基础库/16.过滤器/Filter.cs
@@ -0,0 +1,75 @@
+// // *********************************************************************************
+// // # Project: JFramework
+// // # Unity: 6000.3.5f1
+// // # Author: 云谷千羽
+// // # Version: 1.0.0
+// // # History: 2025-04-30 21:04:34
+// // # Recently: 2025-04-30 21:04:34
+// // # Copyright: 2024, 云谷千羽
+// // # Description: This is an automatically generated comment.
+// // *********************************************************************************
+
+using System.Collections.Generic;
+
+namespace Astraia
+{
+    public static partial class Service
+    {
+        public static partial class Filter
+        {
+            private static readonly Node root = new Node();
+
+            public static void Load(IEnumerable<string> words)
+            {
+                foreach (var word in words)
+                {
+                    Search(word);
+                }
+            }
+
+            private static void Search(string word)
+            {
+                var current = root;
+                foreach (var ch in word)
+                {
+                    if (!current.nodes.TryGetValue(ch, out var node))
+                    {
+                        node = new Node();
+                        current.nodes[ch] = node;
+                    }
+
+                    current = node;
+                }
+
+                current.finish = true;
+            }
+
+            public static string Replace(string text, char mask = '*')
+            {
+                var chars = text.ToCharArray();
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    var current = root;
+                    int j = i;
+                    while (j < chars.Length && current.nodes.TryGetValue(chars[j], out var next))
+                    {
+                        if (next.finish)
+                        {
+                            for (int k = i; k <= j; k++)
+                            {
+                                chars[k] = mask;
+                            }
+
+                            break;
+                        }
+
+                        current = next;
+                        j++;
+                    }
+                }
+
+                return new string(chars);
+            }
+        }
+    }
+}
