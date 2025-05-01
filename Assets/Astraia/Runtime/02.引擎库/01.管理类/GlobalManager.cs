87d80df50b0acf93fb8b622f0acfce0a252b48f8
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-05-01 21:27:49 +0800

    Update GlobalManager.cs

--- Assets/Astraia/Runtime/02.引擎库/01.管理类/GlobalManager.cs
+++ Assets/Astraia/Runtime/02.引擎库/01.管理类/GlobalManager.cs
@@ -24,88 +24,29 @@ namespace Astraia.Common
     public class GlobalManager : MonoBehaviour
     {
         public static GlobalManager Instance;
-
         public Canvas canvas;
-
         public AudioSource sounds;
-
+        
         internal static AudioSetting settings;
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
         internal static AssetBundleManifest manifest;
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
         internal static readonly List<ITimer> timerData = new List<ITimer>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
         internal static readonly List<AudioSource> audioData = new List<AudioSource>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
         internal static readonly Dictionary<string, PackData> clientPacks = new Dictionary<string, PackData>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
         internal static readonly Dictionary<string, PackData> serverPacks = new Dictionary<string, PackData>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
         internal static readonly Dictionary<string, AssetData> assetData = new Dictionary<string, AssetData>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
         internal static readonly Dictionary<string, AssetBundle> assetPack = new Dictionary<string, AssetBundle>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
         internal static readonly Dictionary<string, Task<AssetBundle>> assetTask = new Dictionary<string, Task<AssetBundle>>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
         internal static readonly Dictionary<string, string> assetPath = new Dictionary<string, string>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
-        internal static readonly LiDictionary<Type, ItemTable> itemTable = new LiDictionary<Type, ItemTable>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
-        internal static readonly LiDictionary<Type, NameTable> nameTable = new LiDictionary<Type, NameTable>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
-        internal static readonly LiDictionary<Type, EnumTable> enumTable = new LiDictionary<Type, EnumTable>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
-        internal static readonly LiDictionary<string, IPool> poolData = new LiDictionary<string, IPool>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
         internal static readonly Dictionary<string, GameObject> poolGroup = new Dictionary<string, GameObject>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
-        internal static readonly LiDictionary<Component, AgentData> agentData = new LiDictionary<Component, AgentData>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
-        internal static readonly Dictionary<Type, UIPanel> panelData = new Dictionary<Type, UIPanel>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
         internal static readonly Dictionary<int, RectTransform> layerData = new Dictionary<int, RectTransform>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
-        internal static readonly LiDictionary<string, HashSet<UIPanel>> groupData = new LiDictionary<string, HashSet<UIPanel>>();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-        [Sirenix.OdinInspector.ShowInInspector]
-#endif
+        internal static readonly Dictionary<Type, UIPanel> panelData = new Dictionary<Type, UIPanel>();
         internal static readonly Dictionary<ulong, GameObject> objectData = new Dictionary<ulong, GameObject>();
+        internal static readonly LiDictionary<Type, ItemTable> itemTable = new LiDictionary<Type, ItemTable>();
+        internal static readonly LiDictionary<Type, NameTable> nameTable = new LiDictionary<Type, NameTable>();
+        internal static readonly LiDictionary<Type, EnumTable> enumTable = new LiDictionary<Type, EnumTable>();
+        internal static readonly LiDictionary<string, IPool> poolData = new LiDictionary<string, IPool>();
+        internal static readonly LiDictionary<string, HashSet<UIPanel>> groupData = new LiDictionary<string, HashSet<UIPanel>>();
+        internal static readonly LiDictionary<Component, AgentData> agentData = new LiDictionary<Component, AgentData>();
 
 
         private void Awake()
