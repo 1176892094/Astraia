ffb4d03867a47c78bb7ec3013da7e23201f5cbd2
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-08-04 02:06:15 +0800

    LinkNode->TaskNode

---
+++ Assets/Astraia/Runtime/02.引擎库/17.动态链接/TaskNode.cs
@@ -0,0 +1,134 @@
+// // *********************************************************************************
+// // # Project: Astraia
+// // # Unity: 6000.3.5f1
+// // # Author: 云谷千羽
+// // # Version: 1.0.0
+// // # History: 2025-07-30 16:07:54
+// // # Recently: 2025-07-30 16:07:54
+// // # Copyright: 2024, 云谷千羽
+// // # Description: This is an automatically generated comment.
+// // *********************************************************************************
+
+using System;
+using System.Threading.Tasks;
+
+namespace Astraia
+{
+    [Serializable]
+    public abstract partial class TaskNode
+    {
+        public TaskNode[] nodes;
+        public abstract Task<State> Execute(int id);
+
+        public enum State
+        {
+            Success,
+            Failure
+        }
+
+        protected static class Task
+        {
+            public static readonly Task<State> Success = System.Threading.Tasks.Task.FromResult(State.Success);
+            public static readonly Task<State> Failure = System.Threading.Tasks.Task.FromResult(State.Failure);
+        }
+    }
+
+    public partial class TaskNode
+    {
+        [Serializable]
+        public class Sequence : TaskNode
+        {
+            public override async Task<State> Execute(int id)
+            {
+                foreach (var node in nodes)
+                {
+                    var state = await node.Execute(id);
+                    if (state == State.Failure)
+                    {
+                        return State.Failure;
+                    }
+                }
+
+                return State.Success;
+            }
+        }
+
+        [Serializable]
+        public class Selector : TaskNode
+        {
+            public override async Task<State> Execute(int id)
+            {
+                foreach (var node in nodes)
+                {
+                    var state = await node.Execute(id);
+                    if (state == State.Success)
+                    {
+                        return State.Success;
+                    }
+                }
+
+                return State.Failure;
+            }
+        }
+
+        [Serializable]
+        public class Revealer : TaskNode
+        {
+            public override async Task<State> Execute(int id)
+            {
+                var state = await nodes[Service.Random.Next(nodes.Length)].Execute(id);
+                return state == State.Success ? State.Success : State.Failure;
+            }
+        }
+
+        [Serializable]
+        public class Repeater : TaskNode
+        {
+            public int count;
+
+            public override async Task<State> Execute(int id)
+            {
+                foreach (var node in nodes)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        var state = await node.Execute(id);
+                        if (state == State.Failure)
+                        {
+                            return State.Failure;
+                        }
+                    }
+                }
+
+                return State.Success;
+            }
+        }
+
+        [Serializable]
+        public class WaitTime : TaskNode
+        {
+            public Entity owner;
+            public float waitTime;
+
+            public override async Task<State> Execute(int id)
+            {
+                if (!owner)
+                {
+                    return State.Failure;
+                }
+
+                await owner.Wait(waitTime);
+                foreach (var node in nodes)
+                {
+                    var state = await node.Execute(id);
+                    if (state == State.Failure)
+                    {
+                        return State.Failure;
+                    }
+                }
+
+                return State.Success;
+            }
+        }
+    }
+}
