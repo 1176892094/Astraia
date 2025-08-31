8fb873b93c99a277f1913ca08e39572b7e8e45b7
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-08-31 13:49:25 +0800

    Node->KeyValuePair

--- Assets/Astraia/Runtime/01.基础库/05.容器/List.cs
+++ Assets/Astraia/Runtime/01.基础库/05.容器/List.cs
@@ -16,10 +16,10 @@ using System.Collections.Generic;
 namespace Astraia
 {
     [Serializable]
-    public sealed partial class List<Key, Value> : ICollection<List<Key, Value>.Node>
+    public sealed class List<Key, Value> : ICollection<KeyValuePair<Key, Value>>
     {
-        private Dictionary<Key, LinkedListNode<Node>> items = new Dictionary<Key, LinkedListNode<Node>>();
-        private LinkedList<Node> nodes = new LinkedList<Node>();
+        private Dictionary<Key, LinkedListNode<KeyValuePair<Key, Value>>> items = new();
+        private LinkedList<KeyValuePair<Key, Value>> nodes = new();
         public int Count => nodes.Count;
 
         public Value this[Key key]
@@ -29,34 +29,27 @@ namespace Astraia
             {
                 if (items.TryGetValue(key, out var item))
                 {
-                    var node = item.Value;
-                    node.Value = value;
-                    item.Value = node;
+                    item.Value = new KeyValuePair<Key, Value>(key, value);
                 }
                 else
                 {
-                    items[key] = nodes.AddLast(new Node(key, value));
+                    items[key] = nodes.AddLast(new KeyValuePair<Key, Value>(key, value));
                 }
             }
         }
 
-        public void Reset(IEnumerable<Node> data)
+        public void Reset(IEnumerable<KeyValuePair<Key, Value>> data)
         {
             items.Clear();
             nodes.Clear();
             AddRange(data);
         }
 
-        public void AddRange(IEnumerable<Node> data, Predicate<Node> match = null)
+        public void AddRange(IEnumerable<KeyValuePair<Key, Value>> data, Predicate<KeyValuePair<Key, Value>> match = null)
         {
-            if (data == null)
-            {
-                throw new ArgumentNullException(nameof(data));
-            }
-
             foreach (var node in data)
             {
-                if (match == null || match(node))
+                if (match == null || match.Invoke(node))
                 {
                     Add(node.Key, node.Value);
                 }
@@ -65,37 +58,22 @@ namespace Astraia
 
         public void Add(Key key, Value value)
         {
-            if (items.ContainsKey(key))
-            {
-                throw new ArgumentException(nameof(key));
-            }
-
-            items[key] = nodes.AddLast(new Node(key, value));
+            items.Add(key, nodes.AddLast(new KeyValuePair<Key, Value>(key, value)));
         }
 
         public void AddFirst(Key key, Value value)
         {
-            if (items.ContainsKey(key))
-            {
-                throw new ArgumentException(nameof(key));
-            }
-
-            items[key] = nodes.AddFirst(new Node(key, value));
+            items.Add(key, nodes.AddFirst(new KeyValuePair<Key, Value>(key, value)));
         }
 
         public void Insert(Key origin, Key key, Value value)
         {
-            if (items.ContainsKey(key))
-            {
-                throw new ArgumentException(nameof(key));
-            }
-
             if (!items.TryGetValue(origin, out var target))
             {
                 throw new KeyNotFoundException(nameof(origin));
             }
 
-            items[key] = nodes.AddBefore(target, new Node(key, value));
+            items.Add(key, nodes.AddBefore(target, new KeyValuePair<Key, Value>(key, value)));
         }
 
         public bool Remove(Key key)
@@ -109,13 +87,8 @@ namespace Astraia
             return false;
         }
 
-        public int Remove(Predicate<Node> match)
+        public int Remove(Predicate<KeyValuePair<Key, Value>> match)
         {
-            if (match == null)
-            {
-                throw new ArgumentNullException(nameof(match));
-            }
-
             var count = 0;
             var node = nodes.First;
             while (node != null)
@@ -185,7 +158,7 @@ namespace Astraia
             }
         }
 
-        public IEnumerator<Node> GetEnumerator()
+        public IEnumerator<KeyValuePair<Key, Value>> GetEnumerator()
         {
             var node = nodes.First;
             while (node != null)
@@ -196,11 +169,31 @@ namespace Astraia
             }
         }
 
-        bool ICollection<Node>.IsReadOnly => false;
-        void ICollection<Node>.Add(Node node) => Add(node.Key, node.Value);
-        bool ICollection<Node>.Remove(Node node) => Remove(node.Key);
-        bool ICollection<Node>.Contains(Node node) => nodes.Contains(node);
-        void ICollection<Node>.CopyTo(Node[] array, int index) => nodes.CopyTo(array, index);
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        void ICollection<KeyValuePair<Key, Value>>.Add(KeyValuePair<Key, Value> node)
+        {
+            Add(node.Key, node.Value);
+        }
+
+        bool ICollection<KeyValuePair<Key, Value>>.Remove(KeyValuePair<Key, Value> node)
+        {
+            return Remove(node.Key);
+        }
+
+        bool ICollection<KeyValuePair<Key, Value>>.Contains(KeyValuePair<Key, Value> node)
+        {
+            return ContainsKey(node.Key);
+        }
+
+        void ICollection<KeyValuePair<Key, Value>>.CopyTo(KeyValuePair<Key, Value>[] array, int index)
+        {
+            nodes.CopyTo(array, index);
+        }
+
+        bool ICollection<KeyValuePair<Key, Value>>.IsReadOnly => false;
     }
 }
