33bfa92a883cf63e2e2dc35237aeff84e4e120af
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-09-05 21:32:03 +0800

    Agent.Singleton

---
+++ Assets/Astraia/Runtime/02.引擎库/09.游戏架构/02.代理/Agent.Singleton.cs
@@ -0,0 +1,28 @@
+// // *********************************************************************************
+// // # Project: Astraia
+// // # Unity: 6000.3.5f1
+// // # Author: 云谷千羽
+// // # Version: 1.0.0
+// // # History: 2025-09-05 21:09:38
+// // # Recently: 2025-09-05 21:09:38
+// // # Copyright: 2024, 云谷千羽
+// // # Description: This is an automatically generated comment.
+// // *********************************************************************************
+
+using Astraia.Common;
+
+namespace Astraia
+{
+    public abstract class Singleton<T> : Agent<Entity>, IAgent where T : Singleton<T>
+    {
+        private static T instance;
+
+        public static T Instance => instance;
+
+        void IAgent.Create(Entity owner)
+        {
+            this.owner = owner;
+            instance = (T)this;
+        }
+    }
+}
