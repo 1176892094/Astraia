c8a34c113e6e85f0b56b7dc33aebed36bf12b59b
Charlotte <75679851+1176892094@users.noreply.github.com>
2026-03-16 16:24:17 +0800

    Dictionary

---
+++ Assets/Astraia/Runtime/01.基础库/07.容器类/Dictionary.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Astraia.Core
+{
+    public class Dictionary<TKey> : IDisposable
+    {
+        private readonly Dictionary<Type, IDictionary> Items = new Dictionary<Type, IDictionary>();
+
+        public void Set<T>(TKey key, T value)
+        {
+            GetDict<T>()[key] = value;
+        }
+
+        public void Add<T>(TKey key, T value)
+        {
+            GetDict<T>().Add(key, value);
+        }
+
+        public void Remove<T>(TKey key)
+        {
+            GetDict<T>().Remove(key);
+        }
+
+        public bool ContainsKey<T>(TKey key)
+        {
+            return GetDict<T>().ContainsKey(key);
+        }
+
+        public bool TryGetValue<T>(TKey key, out T value)
+        {
+            return GetDict<T>().TryGetValue(key, out value);
+        }
+
+        public void Clear<T>()
+        {
+            GetDict<T>().Clear();
+        }
+
+        public void Dispose()
+        {
+            foreach (var item in Items.Values)
+            {
+                item.Clear();
+            }
+
+            Items.Clear();
+        }
+
+        private Dictionary<TKey, T> GetDict<T>()
+        {
+            if (!Items.TryGetValue(typeof(T), out var items))
+            {
+                items = new Dictionary<TKey, T>();
+                Items.Add(typeof(T), items);
+            }
+
+            return (Dictionary<TKey, T>)items;
+        }
+    }
+}
