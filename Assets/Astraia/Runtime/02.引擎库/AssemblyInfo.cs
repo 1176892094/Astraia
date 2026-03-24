50f14cab6f6552441713b721a145055a3c2c7168
Charlotte <75679851+1176892094@users.noreply.github.com>
2026-03-24 21:52:13 +0800

    move

--- Assets/Astraia/Runtime/02.引擎库/AssemblyInfo.cs
+++ Assets/Astraia/Runtime/02.引擎库/AssemblyInfo.cs
@@ -16,11 +16,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("编辑器")]
+[assembly: AssemblyTitle("引擎库")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Charlotte")]
-[assembly: AssemblyProduct("编辑器")]
+[assembly: AssemblyProduct("引擎库")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
