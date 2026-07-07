1b8b1f277619c7a78f1bc815713ace42231a84c1
Charlotte <75679851+1176892094@users.noreply.github.com>
2026-07-08 03:18:59 +0800

    Update Extensions.cs

--- Assets/Astraia/Runtime/引擎库/定义类型/Extensions.cs
+++ Assets/Astraia/Runtime/引擎库/定义类型/Extensions.cs
@@ -214,129 +214,129 @@ namespace Astraia.Core
             return Tween.Create(new AsyncAdaptor(current), duration);
         }
 
-        public static Tween DOMoveX(this Transform transform, float endValue, float duration)
+        public static Tween DOMoveX(this Component component, float endValue, float duration)
         {
-            var position = transform.position;
-            return transform.Play(duration).OnUpdate(progress =>
+            var position = component.transform.position;
+            return component.transform.Play(duration).OnUpdate(progress =>
             {
                 var positionX = Mathf.Lerp(position.x, endValue, progress);
-                transform.position = new Vector3(positionX, position.y, position.z);
+                component.transform.position = new Vector3(positionX, position.y, position.z);
             });
         }
 
-        public static Tween DOMoveY(this Transform transform, float endValue, float duration)
+        public static Tween DOMoveY(this Component component, float endValue, float duration)
         {
-            var position = transform.position;
-            return transform.Play(duration).OnUpdate(progress =>
+            var position = component.transform.position;
+            return component.Play(duration).OnUpdate(progress =>
             {
                 var positionY = Mathf.Lerp(position.y, endValue, progress);
-                transform.position = new Vector3(position.x, positionY, position.z);
+                component.transform.position = new Vector3(position.x, positionY, position.z);
             });
         }
 
-        public static Tween DOMoveZ(this Transform transform, float endValue, float duration)
+        public static Tween DOMoveZ(this Component component, float endValue, float duration)
         {
-            var position = transform.position;
-            return transform.Play(duration).OnUpdate(progress =>
+            var position = component.transform.position;
+            return component.Play(duration).OnUpdate(progress =>
             {
                 var positionZ = Mathf.Lerp(position.z, endValue, progress);
-                transform.position = new Vector3(position.x, position.y, positionZ);
+                component.transform.position = new Vector3(position.x, position.y, positionZ);
             });
         }
 
-        public static Tween DOMove(this Transform transform, Vector3 endValue, float duration)
+        public static Tween DOMove(this Component component, Vector3 endValue, float duration)
         {
-            var position = transform.position;
-            return transform.Play(duration).OnUpdate(progress =>
+            var position = component.transform.position;
+            return component.Play(duration).OnUpdate(progress =>
             {
                 var positionX = Mathf.Lerp(position.x, endValue.x, progress);
                 var positionY = Mathf.Lerp(position.y, endValue.y, progress);
                 var positionZ = Mathf.Lerp(position.z, endValue.z, progress);
-                transform.position = new Vector3(positionX, positionY, positionZ);
+                component.transform.position = new Vector3(positionX, positionY, positionZ);
             });
         }
 
-        public static Tween DORotateX(this Transform transform, float endValue, float duration)
+        public static Tween DORotateX(this Component component, float endValue, float duration)
         {
-            var rotation = transform.rotation.eulerAngles;
-            return transform.Play(duration).OnUpdate(progress =>
+            var rotation = component.transform.rotation.eulerAngles;
+            return component.Play(duration).OnUpdate(progress =>
             {
                 var rotationX = Mathf.LerpAngle(rotation.x, endValue, progress);
-                transform.rotation = Quaternion.Euler(rotationX, rotation.y, rotation.z);
+                component.transform.rotation = Quaternion.Euler(rotationX, rotation.y, rotation.z);
             });
         }
 
-        public static Tween DORotateY(this Transform transform, float endValue, float duration)
+        public static Tween DORotateY(this Component component, float endValue, float duration)
         {
-            var rotation = transform.rotation.eulerAngles;
-            return transform.Play(duration).OnUpdate(progress =>
+            var rotation = component.transform.rotation.eulerAngles;
+            return component.Play(duration).OnUpdate(progress =>
             {
                 var rotationY = Mathf.LerpAngle(rotation.y, endValue, progress);
-                transform.rotation = Quaternion.Euler(rotation.x, rotationY, rotation.z);
+                component.transform.rotation = Quaternion.Euler(rotation.x, rotationY, rotation.z);
             });
         }
 
-        public static Tween DORotateZ(this Transform transform, float endValue, float duration)
+        public static Tween DORotateZ(this Component component, float endValue, float duration)
         {
-            var rotation = transform.rotation.eulerAngles;
-            return transform.Play(duration).OnUpdate(progress =>
+            var rotation = component.transform.rotation.eulerAngles;
+            return component.Play(duration).OnUpdate(progress =>
             {
                 var rotationZ = Mathf.LerpAngle(rotation.z, endValue, progress);
-                transform.rotation = Quaternion.Euler(rotation.x, rotation.y, rotationZ);
+                component.transform.rotation = Quaternion.Euler(rotation.x, rotation.y, rotationZ);
             });
         }
 
-        public static Tween DORotate(this Transform transform, Vector3 endValue, float duration)
+        public static Tween DORotate(this Component component, Vector3 endValue, float duration)
         {
-            var rotation = transform.rotation.eulerAngles;
-            return transform.Play(duration).OnUpdate(progress =>
+            var rotation = component.transform.rotation.eulerAngles;
+            return component.Play(duration).OnUpdate(progress =>
             {
                 var rotationX = Mathf.Lerp(rotation.x, endValue.x, progress);
                 var rotationY = Mathf.Lerp(rotation.y, endValue.y, progress);
                 var rotationZ = Mathf.Lerp(rotation.z, endValue.z, progress);
-                transform.rotation = Quaternion.Euler(rotationX, rotationY, rotationZ);
+                component.transform.rotation = Quaternion.Euler(rotationX, rotationY, rotationZ);
             });
         }
 
-        public static Tween DOScaleX(this Transform transform, float endValue, float duration)
+        public static Tween DOScaleX(this Component component, float endValue, float duration)
         {
-            var localScale = transform.localScale;
-            return transform.Play(duration).OnUpdate(progress =>
+            var localScale = component.transform.localScale;
+            return component.Play(duration).OnUpdate(progress =>
             {
                 var localScaleX = Mathf.Lerp(localScale.x, endValue, progress);
-                transform.localScale = new Vector3(localScaleX, localScale.y, localScale.z);
+                component.transform.localScale = new Vector3(localScaleX, localScale.y, localScale.z);
             });
         }
 
-        public static Tween DOScaleY(this Transform transform, float endValue, float duration)
+        public static Tween DOScaleY(this Component component, float endValue, float duration)
         {
-            var localScale = transform.localScale;
-            return transform.Play(duration).OnUpdate(progress =>
+            var localScale = component.transform.localScale;
+            return component.Play(duration).OnUpdate(progress =>
             {
                 var localScaleY = Mathf.Lerp(localScale.y, endValue, progress);
-                transform.localScale = new Vector3(localScale.x, localScaleY, localScale.z);
+                component.transform.localScale = new Vector3(localScale.x, localScaleY, localScale.z);
             });
         }
 
-        public static Tween DOScaleZ(this Transform transform, float endValue, float duration)
+        public static Tween DOScaleZ(this Component component, float endValue, float duration)
         {
-            var localScale = transform.localScale;
-            return transform.Play(duration).OnUpdate(progress =>
+            var localScale = component.transform.localScale;
+            return component.Play(duration).OnUpdate(progress =>
             {
                 var localScaleZ = Mathf.Lerp(localScale.z, endValue, progress);
-                transform.localScale = new Vector3(localScale.x, localScale.y, localScaleZ);
+                component.transform.localScale = new Vector3(localScale.x, localScale.y, localScaleZ);
             });
         }
 
-        public static Tween DOScale(this Transform transform, Vector3 endValue, float duration)
+        public static Tween DOScale(this Component component, Vector3 endValue, float duration)
         {
-            var localScale = transform.localScale;
-            return transform.Play(duration).OnUpdate(progress =>
+            var localScale = component.transform.localScale;
+            return component.Play(duration).OnUpdate(progress =>
             {
                 var localScaleX = Mathf.Lerp(localScale.x, endValue.x, progress);
                 var localScaleY = Mathf.Lerp(localScale.y, endValue.y, progress);
                 var localScaleZ = Mathf.Lerp(localScale.z, endValue.z, progress);
-                transform.localScale = new Vector3(localScaleX, localScaleY, localScaleZ);
+                component.transform.localScale = new Vector3(localScaleX, localScaleY, localScaleZ);
             });
         }
     }
