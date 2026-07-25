6c78704dec08e499fa4739d14c6df4787690200b
Charlotte <75679851+1176892094@users.noreply.github.com>
2026-07-25 23:54:40 +0800

    Update Entity.cs

--- Assets/Astraia/Runtime/基础库/实例类型/Entity.cs
+++ Assets/Astraia/Runtime/基础库/实例类型/Entity.cs
@@ -257,28 +257,53 @@ namespace Astraia
     }
 
     [Serializable]
-    public sealed class StateMachine<T>
+    public sealed class StateMachine
     {
-        private Dictionary<T, IState> states = new();
+        private Dictionary<int, IState> states = new();
         private IState state;
+        private IState other;
+        private int index;
 
-        public void Create<TState>(object owner, T key)
+        public void Create<TState>(object owner, int value)
         {
             var item = (IState)Activator.CreateInstance<TState>();
             item.Acquire(owner);
-            states[key] = item;
+            states[value] = item;
         }
 
-        public void Switch(T key)
+        public void Switch(int value)
         {
             state?.OnExit();
-            states.TryGetValue(key, out state);
+            states.TryGetValue(value, out state);
             state?.OnEnter();
         }
 
         public void Update()
         {
             state?.OnUpdate();
+        }
+
+        public void Update(int value)
+        {
+            if (other == state)
+            {
+                return;
+            }
+
+            if (index != value)
+            {
+                other?.OnExit();
+            }
+
+            states.TryGetValue(value, out other);
+
+            if (index != value)
+            {
+                other?.OnEnter();
+            }
+
+            index = value;
+            other?.OnUpdate();
         }
 
         public void Clear()
